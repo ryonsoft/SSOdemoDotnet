5792d378ac317d1d59b0f8e430d3e2cf9a26dd1f
ryonsoft <ryonsoft@gmail.com>
2023-10-23 16:41:49 +0900

    first commit

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TrainingADIntegration.Data;
+
+[assembly: HostingStartup(typeof(TrainingADIntegration.Areas.Identity.IdentityHostingStartup))]
+namespace TrainingADIntegration.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<TrainingADIntegrationContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("TrainingADIntegrationContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<TrainingADIntegrationContext>();
+            });
+        }
+    }
+}
